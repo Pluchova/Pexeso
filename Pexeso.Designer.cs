cb6c568d5dded8f5379fe1106cc41f07bb57a754
Nikča <Nika@DESKTOP-PCADTG4>
2024-01-08 20:09:37 +0100

    Add project files.

---
+++ Pexeso.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Pexeso
+{
+    partial class Pexeso
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            casovac = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 255);
+            button1.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.FromArgb(255, 192, 255);
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 91);
+            button1.TabIndex = 0;
+            button1.Text = "J";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += pexeso_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 192, 255);
+            button2.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.FromArgb(255, 192, 255);
+            button2.Location = new Point(114, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(96, 91);
+            button2.TabIndex = 1;
+            button2.Text = "7";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += pexeso_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 192, 255);
+            button3.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.FromArgb(255, 192, 255);
+            button3.Location = new Point(216, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(96, 91);
+            button3.TabIndex = 2;
+            button3.Text = "6";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += pexeso_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(255, 192, 255);
+            button4.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.FromArgb(255, 192, 255);
+            button4.Location = new Point(318, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(96, 91);
+            button4.TabIndex = 3;
+            button4.Text = "!";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += pexeso_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(255, 192, 255);
+            button5.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = Color.FromArgb(255, 192, 255);
+            button5.Location = new Point(12, 109);
+            button5.Name = "button5";
+            button5.Size = new Size(96, 91);
+            button5.TabIndex = 4;
+            button5.Text = "1";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += pexeso_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(255, 192, 255);
+            button6.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.ForeColor = Color.FromArgb(255, 192, 255);
+            button6.Location = new Point(114, 109);
+            button6.Name = "button6";
+            button6.Size = new Size(96, 91);
+            button6.TabIndex = 5;
+            button6.Text = "T";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += pexeso_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(255, 192, 255);
+            button7.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.ForeColor = Color.FromArgb(255, 192, 255);
+            button7.Location = new Point(216, 109);
+            button7.Name = "button7";
+            button7.Size = new Size(96, 91);
+            button7.TabIndex = 6;
+            button7.Text = "Q";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += pexeso_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(255, 192, 255);
+            button8.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.ForeColor = Color.FromArgb(255, 192, 255);
+            button8.Location = new Point(318, 109);
+            button8.Name = "button8";
+            button8.Size = new Size(96, 91);
+            button8.TabIndex = 7;
+            button8.Text = "T";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += pexeso_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(255, 192, 255);
+            button9.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.ForeColor = Color.FromArgb(255, 192, 255);
+            button9.Location = new Point(318, 206);
+            button9.Name = "button9";
+            button9.Size = new Size(96, 91);
+            button9.TabIndex = 8;
+            button9.Text = "1";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += pexeso_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(255, 192, 255);
+            button10.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button10.ForeColor = Color.FromArgb(255, 192, 255);
+            button10.Location = new Point(216, 206);
+            button10.Name = "button10";
+            button10.Size = new Size(96, 91);
+            button10.TabIndex = 9;
+            button10.Text = "J";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += pexeso_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(255, 192, 255);
+            button11.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button11.ForeColor = Color.FromArgb(255, 192, 255);
+            button11.Location = new Point(114, 206);
+            button11.Name = "button11";
+            button11.Size = new Size(96, 91);
+            button11.TabIndex = 10;
+            button11.Text = "Q";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += pexeso_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(255, 192, 255);
+            button12.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button12.ForeColor = Color.FromArgb(255, 192, 255);
+            button12.Location = new Point(12, 206);
+            button12.Name = "button12";
+            button12.Size = new Size(96, 91);
+            button12.TabIndex = 11;
+            button12.Text = "!";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += pexeso_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.FromArgb(255, 192, 255);
+            button13.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button13.ForeColor = Color.FromArgb(255, 192, 255);
+            button13.Location = new Point(12, 303);
+            button13.Name = "button13";
+            button13.Size = new Size(96, 91);
+            button13.TabIndex = 12;
+            button13.Text = "Z";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += pexeso_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.FromArgb(255, 192, 255);
+            button14.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button14.ForeColor = Color.FromArgb(255, 192, 255);
+            button14.Location = new Point(114, 303);
+            button14.Name = "button14";
+            button14.Size = new Size(96, 91);
+            button14.TabIndex = 13;
+            button14.Text = "6";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += pexeso_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.FromArgb(255, 192, 255);
+            button15.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button15.ForeColor = Color.FromArgb(255, 192, 255);
+            button15.Location = new Point(216, 303);
+            button15.Name = "button15";
+            button15.Size = new Size(96, 91);
+            button15.TabIndex = 14;
+            button15.Text = "7";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += pexeso_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.FromArgb(255, 192, 255);
+            button16.Font = new Font("Wingdings", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            button16.ForeColor = Color.FromArgb(255, 192, 255);
+            button16.Location = new Point(318, 303);
+            button16.Name = "button16";
+            button16.Size = new Size(96, 91);
+            button16.TabIndex = 15;
+            button16.Text = "Z";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += pexeso_Click;
+            // 
+            // casovac
+            // 
+            casovac.Enabled = true;
+            casovac.Interval = 1000;
+            casovac.Tick += timer1_Tick;
+            // 
+            // Pexeso
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(426, 405);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Pexeso";
+            StartPosition = FormStartPosition.Manual;
+            Text = "Pexeso";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private System.Windows.Forms.Timer casovac;
+    }
+}
---
+++ Pexeso.cs
@@ -0,0 +1,125 @@
+namespace Pexeso
+{
+    public partial class Pexeso : Form
+    {
+        public Button prvniStisknuteTlacitko;
+        public Button druheStisknuteTlacitko;
+        private int _cas;
+        private int _pocetTahu = 1;
+        public Pexeso()
+        {
+            InitializeComponent();
+            Button[] buttons = new Button[16];
+            int i = 0;
+
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button)
+                {
+                    buttons[i] = c as Button;
+                    i++;
+                }
+            }
+
+            zamichej(buttons);
+        }
+
+        private void pexeso_Click(object sender, EventArgs e)
+        {
+            // pøetypování na button
+            Button button = (Button)sender;
+
+            zkontroluj_viteze();
+
+            //když je na tlaèítko kliknuté provede se:
+            if (button != null)
+
+            {   // pokud je text tlaèítka již èerný - na tlaèítko už bylo kliknuté - nic se nedìje
+                if (button.ForeColor == Color.Black)
+                    return;
+                //  první tlaèítko
+                if (prvniStisknuteTlacitko == null)
+                {
+                    prvniStisknuteTlacitko = button;
+                    prvniStisknuteTlacitko.ForeColor = Color.Black;
+                    return;
+                }
+                // druhé tlaèítko
+                if (druheStisknuteTlacitko == null)
+                {
+                    druheStisknuteTlacitko = button;
+                    druheStisknuteTlacitko.ForeColor = Color.Black;
+                    return;
+                }
+                //poèítání tahù
+                _pocetTahu++;
+                casovac.Start();
+                // když jsou obì tlaèítka stisknutá - porovná se text
+                if (prvniStisknuteTlacitko != null && druheStisknuteTlacitko != null)
+                {   // rovnají se - resetují se hodnoty
+                    if (prvniStisknuteTlacitko.Text == druheStisknuteTlacitko.Text)
+                    {
+                        prvniStisknuteTlacitko = null;
+                        druheStisknuteTlacitko = null;
+
+                    }
+                    else
+                    {  // obnoví se pùvodní barva když se nerovnají
+                        prvniStisknuteTlacitko.ForeColor = prvniStisknuteTlacitko.BackColor;
+                        druheStisknuteTlacitko.ForeColor = druheStisknuteTlacitko.BackColor;
+
+
+                    }
+
+                    //reset promìnných 
+                    prvniStisknuteTlacitko = null;
+                    druheStisknuteTlacitko = null;
+
+                }
+            }
+
+        }
+        private void zkontroluj_viteze()
+        {   //projde všechny tlaèítka typu button
+            foreach (Button button in Controls.OfType<Button>())
+            {
+                if (button.ForeColor != Color.Black)
+                {
+                    Console.WriteLine("Tlaèítko není èerné: " + button.Text);
+                    return; // Pokud alespoò jedno tlaèítko není èerné, není výhra
+                }
+            }
+            //zastaví se èasovaè a vypíše se hláška
+            casovac.Stop();
+            MessageBox.Show($"Vyhrál jsi za {_cas} sekund, za použití {_pocetTahu} tahù.");
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {  //poèítání èasu
+            _cas++;
+        }
+        static void zamichej(Button[] poleTlacitek)
+        {
+            //deklarace promìnné n
+            int n = poleTlacitek.Length - 1;
+            // vytvoøení random generátoru
+            Random generator = new Random();
+            // dokud je n vìtší než nula cyklus
+            while (n > 0)
+            { // výbìr náhodného prvku od 0 do n
+                int nahodnyPrvek = generator.Next(0, n);
+                //deklarace doèasnné promìnné, která se rovná prvku na pozici n
+                string docasnaPromenna = poleTlacitek[n].Text;
+                //na pozici n umístìní èísla z pozice náhodnì vybraného prvku
+                poleTlacitek[n].Text = poleTlacitek[nahodnyPrvek].Text;
+                //na pozici náhodnì vybraného prvku umístìní èísla z pozice n = dokonèené pøehození
+                poleTlacitek[nahodnyPrvek].Text = docasnaPromenna;
+                // snížení n o 1
+                n--;
+            }
+        }
+
+
+    }
+}
